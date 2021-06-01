9ea207dd078d068c4ada80a23fe26ef3c3b475e3
sahrulgobel <85119051+sahrulgobel@users.noreply.github.com>
2021-06-01 03:44:10 -0700

    Menambahkan anggaran, tujuan project dan mulai dan selesai project

--- rencanapenyelesaiproject.php
+++ rencanapenyelesaiproject.php
@@ -1,7 +1,10 @@
 RENCANA PENYELESAIAN 
-- Informasi Anggaran
+- Informasi Anggaran Rp. 30.000.000
   ^akan melakukan pertemuan langsung atau zoom
-- Tanggal Mulai Pengerjaan
-- Tanggal Selesai Pengerjaan
+- Tanggal Mulai Pengerjaan 1 Maret 
+- Tanggal Selesai Pengerjaan 30 April 2021
 - Manajer Project Aslan Onu
-- Tujuan project yang di buat
+- Tujuan project yang di buat Membuat sebuah aplikasi berbasis WEB untuk membantu pelayanan administrasi di sekolah Madrasah Tsanawiyah Negeri 1 Kota Gorontalo. 
+Dimana dalam pembuatan aplikasi kami menyediakan pelayanan yang dibutuhkan seperti pembuatan SK, surat masuk dan keluar, legalisir (Ijazah, SKHUN, Raport), 
+surat mutasi dan surat keterangan siswa yang nantinya dapat membantu dan mempermudah siswa, guru dan orang tua. 
+Dengan di buatnya aplikasis berbasis web ini dapat menghemat waktu serta biaya dan lebih efisien.
