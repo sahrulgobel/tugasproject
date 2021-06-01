09725c6027d233ad830258f400708c710003bdfe
aslanonu <81285472+aslanonu@users.noreply.github.com>
2021-05-31 19:39:08 -0700

    Menghapus nomor kontrak

--- tugasproject.php
+++ tugasproject.php
@@ -2,7 +2,6 @@ RENCANA PEMBUATAN TUGAS PROJECT
 - Nama Project Sistem Informasi Administrasi Di Mts N 1 Kota Gorontalo
 - Tanggal Mulai Project
 - Tanggal Akhir Project
-- Nomor Kontrak
 - Manajer Project
 - Informasi Anggaran
 - Tujuan Project
